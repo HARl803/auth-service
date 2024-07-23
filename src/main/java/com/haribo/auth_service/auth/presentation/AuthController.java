88283467301139c640d7097bac5ca449aecef8e8
박소미 <fsm1204@naver.com>
2024-07-23 15:10:26 +0900

    🗃 db: `auth_member` 엔티티와 RDB를 연결하기

---
+++ src/main/java/com/haribo/auth_service/auth/presentation/AuthController.java
@@ -0,0 +1,10 @@
+package com.haribo.auth_service.auth.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+}
