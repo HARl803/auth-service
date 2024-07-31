90d1cd036eb69677be53ea2da8433aa7e912be2b
fsm12 <fsm1204@naver.com>
2024-07-31 10:58:44 +0900

    ✨ feat[#2]: AuthMember와 ProfileMember 엔티티 설정

--- src/main/java/com/haribo/auth_service/auth/domain/repository/AuthMemberRepository.java
+++ src/main/java/com/haribo/auth_service/auth/domain/repository/AuthMemberRepository.java
@@ -4,6 +4,9 @@ import com.haribo.auth_service.auth.domain.AuthMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthMemberRepository extends JpaRepository<AuthMember, String> {
+    Optional<AuthMember> findByKakaoUid(Long kakaoUid);
 }
