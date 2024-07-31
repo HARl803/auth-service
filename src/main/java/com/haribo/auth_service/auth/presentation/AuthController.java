953f048609d959d429716cd6bbb317884d7e162e
fsm12 <fsm1204@naver.com>
2024-07-31 10:51:09 +0900

    ✨ feat[#2]: 카카오 로그인이 정상 처리 되었을 때 받은 인가코드로 AccesToken 발급

--- src/main/java/com/haribo/auth_service/auth/presentation/AuthController.java
+++ src/main/java/com/haribo/auth_service/auth/presentation/AuthController.java
@@ -1,9 +1,12 @@
 package com.haribo.auth_service.auth.presentation;
 
 import com.haribo.auth_service.auth.application.service.AuthService;
+import com.haribo.auth_service.auth.presentation.response.KakaoMemberResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -21,5 +24,12 @@ public class AuthController {
         logger.info("kakao-login 화면 진입");
         String kakaoLoginUrl = authService.getKakaoLoginUrl();
         return new RedirectView(kakaoLoginUrl);
+    }
+
+    @GetMapping("/redirect-kakaologin")
+    public ResponseEntity<KakaoMemberResponse> kakaoLogin(HttpServletRequest request) throws Exception {
+        logger.info("kakao-login 후에 진행할 로직");
+        KakaoMemberResponse kakaoMemberResponse = authService.getKakaoInfoWithCode(request.getParameter("code"));
+        return ResponseEntity.ok().body(kakaoMemberResponse);
     }
 }
