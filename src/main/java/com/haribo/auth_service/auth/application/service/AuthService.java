53f38f6b5cb933da0e4d3c609739c69661290adb
fsm12 <fsm1204@naver.com>
2024-07-31 10:51:09 +0900

    ✨ feat[#2]: 카카오 로그인이 정상 처리 되었을 때 받은 인가코드로 AccesToken 발급

--- src/main/java/com/haribo/auth_service/auth/application/service/AuthService.java
+++ src/main/java/com/haribo/auth_service/auth/application/service/AuthService.java
@@ -1,5 +1,8 @@
 package com.haribo.auth_service.auth.application.service;
 
+import com.haribo.auth_service.auth.presentation.response.KakaoMemberResponse;
+
 public interface AuthService {
     String getKakaoLoginUrl();
+    KakaoMemberResponse getKakaoInfoWithCode(String code) throws Exception;
 }
