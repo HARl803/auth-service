6184afb0397d5c79db03c9422b18d894aee8d7dc
fsm12 <fsm1204@naver.com>
2024-07-31 11:20:21 +0900

    ✨ feat[#2]: 로그인 구현에 필요한 Dto와 Enum 선언

---
+++ src/main/java/com/haribo/auth_service/global/enums/MemberStatus.java
@@ -0,0 +1,12 @@
+package com.haribo.auth_service.global.enums;
+
+/**
+ ACTIVE : 활성화
+ INACTIVE : 비활성화 (휴면계정)
+ DELETED : 탈퇴
+ */
+public enum MemberStatus {
+    ACTIVE,
+    INACTIVE,
+    DELETED
+}
