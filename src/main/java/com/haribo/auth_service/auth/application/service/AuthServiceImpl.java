f69e130457f601039f79c98e3d13104d8b1109bc
fsm12 <fsm1204@naver.com>
2024-07-31 10:59:09 +0900

    ✨ feat[#2]: 발급받은 AccessToken으로 카카오에 저장된 멤버 정보 가져오기

--- src/main/java/com/haribo/auth_service/auth/application/service/AuthServiceImpl.java
+++ src/main/java/com/haribo/auth_service/auth/application/service/AuthServiceImpl.java
@@ -1,6 +1,13 @@
 package com.haribo.auth_service.auth.application.service;
 
+import com.haribo.auth_service.auth.application.dto.kakaoapi.KakaoMemberDto;
+import com.haribo.auth_service.auth.domain.AuthMember;
+import com.haribo.auth_service.auth.domain.repository.AuthMemberRepository;
 import com.haribo.auth_service.auth.presentation.response.KakaoMemberResponse;
+import com.haribo.auth_service.member.domain.ProfileMember;
+import com.haribo.auth_service.member.domain.repository.ProfileMemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -16,10 +23,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
+    private final AuthMemberRepository authMemberRepository;
+    private final ProfileMemberRepository profileMemberRepository;
 
     @Value("${kakao.client.id}")
     private String KAKAO_CLIENT_ID;
@@ -32,6 +48,10 @@ public class AuthServiceImpl implements AuthService {
 
     @Value("${kakao.redirect.logout}")
     private String KAKAO_REDIRECT_LOGOUT;
+
+    @Value("${base.profile-image}")
+    private String BASE_PROFILE_IMAGE;
+
 
     private static final String KAKAO_AUTH_URI = "https://kauth.kakao.com";
     private static final String KAKAO_API_URI = "https://kapi.kakao.com";
@@ -58,7 +78,7 @@ public class AuthServiceImpl implements AuthService {
         JSONObject jsonObj = getMemberInfoWithToken(accessToken);
 
         logger.info("3. 가져온 정보로 가입 후 로그인 or 로그인을 진행하기");
-
+        KakaoMemberDto kakaoMemberDto = registerAndLoginIfNeeded(jsonObj);
 
         logger.info("4. 응답형식(KakaoMemberResponse)에 맞게 리턴하기");
 
@@ -108,6 +128,76 @@ public class AuthServiceImpl implements AuthService {
 
         JSONParser jsonParser = new JSONParser();
         return (JSONObject) jsonParser.parse(response.getBody());
+    }
+
+    private KakaoMemberDto registerAndLoginIfNeeded(JSONObject jsonObj) throws Exception {
+        JSONObject account = (JSONObject) jsonObj.get("kakao_account");
+        JSONObject profile = (JSONObject) account.get("profile");
+
+        long kakaoId = (long) jsonObj.get("id");
+        String email = String.valueOf(account.get("email"));
+        String nickName = String.valueOf(profile.get("nickname"));
+        String profileImage = String.valueOf(profile.get("profile_image_url"));
+
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        HttpSession session = request.getSession();
+
+        logger.info("가입여부 판단");
+        AuthMember authMember;
+        ProfileMember profileMember;
+
+        Optional<AuthMember> optionalAuthMember = authMemberRepository.findByKakaoUid(kakaoId);
+        if (optionalAuthMember.isPresent()) {
+            logger.info("이미 가입한 유저이므로 DB 에서 가져오기");
+            authMember = optionalAuthMember.get();
+            authMember.setLastLoginDate(LocalDateTime.now());
+            authMemberRepository.save(authMember);
+
+            profileMember = authMember.getProfileMember();
+        }else{
+            logger.info("신규 멤버이므로 가입 처리하기");
+            authMember = saveNewAuthMember(kakaoId);
+            profileMember = saveNewProfileMember(authMember, email, nickName, profileImage);
+        }
+
+        logger.info("세션에 로그인한 멤버 정보 저장하기");
+        session.setAttribute("authMember", authMember);
+        session.setAttribute("profileMember", profileMember);
+
+        return KakaoMemberDto.builder()
+                .authMember(authMember)
+                .profileMember(profileMember)
+                .build();
+    }
+
+    private AuthMember saveNewAuthMember(long kakaoUid) {
+        String memberUid = String.valueOf(UUID.randomUUID());
+
+        AuthMember authMember = AuthMember.builder()
+                .memberUid(memberUid)
+                .kakaoUid(kakaoUid)
+                .lastLoginDate(LocalDateTime.now())
+                .build();
+        authMemberRepository.save(authMember);
+
+        return authMemberRepository.findById(memberUid).orElseThrow(() -> new RuntimeException("AuthMember not found"));
+    }
+
+    private ProfileMember saveNewProfileMember(AuthMember authMember, String email, String nickname, String profileImage){
+        ProfileMember profileMember = ProfileMember.builder()
+                .profileId(authMember.getMemberUid())
+                .authMember(authMember)
+                .name(nickname)
+                .nickName(nickname)
+                .email(email)
+                .profileImage(profileImage.equals("null") ? BASE_PROFILE_IMAGE : profileImage)
+                .memberStatus(com.haribo.auth_service.global.enums.MemberStatus.ACTIVE)
+                .build();
+
+        profileMemberRepository.save(profileMember);
+
+        return profileMemberRepository.findById(authMember.getMemberUid()).orElseThrow(() -> new RuntimeException("ProfileMember not found"));
     }
 }
 
