e0ac8f66808a9d93a914095537daf6e6ee084c03
fsm12 <fsm1204@naver.com>
2024-08-05 10:39:20 +0900

    ✨ feat[#2]: 로그인한 사용자 정보 리턴

--- src/main/java/com/haribo/auth_service/auth/presentation/AuthController.java
+++ src/main/java/com/haribo/auth_service/auth/presentation/AuthController.java
@@ -1,12 +1,15 @@
 package com.haribo.auth_service.auth.presentation;
 
+import com.haribo.auth_service.auth.application.dto.kakaoapi.KakaoMemberDto;
 import com.haribo.auth_service.auth.application.service.AuthService;
 import com.haribo.auth_service.auth.presentation.response.KakaoMemberResponse;
+import com.haribo.auth_service.member.domain.ProfileMember;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
@@ -50,11 +53,31 @@ public class AuthController {
         return new RedirectView(kakaoLogoutUrl);
     }
 
-    @GetMapping("/redirect-kakalogout")
+    @GetMapping("/redirect-kakaologout")
     public RedirectView kakalogout() {
         logger.info("kakao-logout 후에 진행할 로직");
         // TODO : 배포 후 사이트 주소 넣을 예정
-        return new RedirectView();
+        return new RedirectView("http://localhost:8080/");
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<KakaoMemberDto> getUserProfile(HttpServletRequest request) {
+        logger.info("로그인한 멤버의 정보를 가져오는 API 호출");
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        ProfileMember profileMember = (ProfileMember) session.getAttribute("profileMember");
+        if(profileMember == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        KakaoMemberDto kakaoMemberDto = KakaoMemberDto.builder()
+                .profileMember(profileMember)
+                .build();
+
+        return ResponseEntity.ok().body(kakaoMemberDto);
     }
 
 }
